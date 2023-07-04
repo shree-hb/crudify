d19243df2db38b0dfb8b9e736923a449555f3e0b
shree-hb <107175680+shree-hb@users.noreply.github.com>
2023-07-04 15:43:15 +0530

    Adding engine code

--- lib/crudify.rb
+++ lib/crudify.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
+
+require "active_support/dependencies"
+
+require 'crudify/engine'
 require_relative "crudify/version"
 
 module Crudify
   class Error < StandardError; end
   # Your code goes here...
+
+
+  def self.mounted_path
+    ::Crud::Engine.mounted_path
+  end
+
 end
--- lib/crudify/engine.rb
+++ lib/crudify/engine.rb
@@ -1,15 +1,18 @@
+require 'rails'
+
 module Crudify
   class Engine < ::Rails::Engine
     isolate_namespace Crudify
 
     # Load engine dependencies
     require "crudify/crud_controller"
+    require "active_support/dependencies"
 
-    # Mount engine routes
-    initializer "crudify.routes" do
-      Crudify::Engine.routes.draw do
-        get "crud", to: "crud#index"
-      end
-    end
+    # # Mount engine routes
+    # initializer "crudify.routes" do
+    #   Crudify::Engine.routes.draw do
+    #     get "crud", to: "crud#index"
+    #   end
+    # end
   end
 end
