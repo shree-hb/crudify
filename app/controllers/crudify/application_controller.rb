0de50c95305c31589b312dd7c10f5357a0667114
chintamani@higgsbosonhealth.com <chintamani@higgsbosonhealth.com>
2023-08-08 14:34:32 +0530

    Committed all the changes

--- app/controllers/crudify/application_controller.rb
+++ app/controllers/crudify/application_controller.rb
@@ -2,5 +2,8 @@ module Crudify
   #class ApplicationController < ActionController::Base
   class ApplicationController < ::MmsApiController
     protect_from_forgery with: :exception
+    skip_before_action :authenticate_user! , :if => proc {!Rails.application.config.content_engine[:is_auth_enabled]}
+
+
   end
 end
